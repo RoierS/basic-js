93296ef03f67297caf987d290cdc5678f6342f20
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-04-15 08:34:23 +0200

    feat: done extended repeater, recursive depth

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = `${str}`;
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const addition = String(options.addition === undefined ? '' : options.addition);
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+  const AddPart = Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+
+  let result = Array(repeatTimes).fill(str + AddPart).join(separator);
+  
+  return result;
 }
 
 module.exports = {
